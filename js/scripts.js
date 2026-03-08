7da6c845563a9fec8e4fb7d68f05f87bb0ba8061
shohelAhmed <shohailislamshohel@gmail.com>
2026-03-08 17:05:22 +0600

    Last version of revision

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
 
 	const menuBtn = document.getElementById("menuBtn");
@@ -10,30 +7,28 @@ document.addEventListener("DOMContentLoaded", function () {
 	const sidebarLinks = document.querySelectorAll("#sidebar a");
 
 	menuBtn.addEventListener("click", () => {
-
-	sidebar.classList.toggle("active");
-	overlay.classList.toggle("active");
-	menuBtn.classList.toggle("active");
-
+		sidebar.classList.toggle("active");
+		overlay.classList.toggle("active");
+		menuBtn.classList.toggle("active");
 	});
 
 	overlay.addEventListener("click", closeSidebar);
-	closeBtn.addEventListener("click", closeSidebar);
 
-	sidebarLinks.forEach(link=>{
-	link.addEventListener("click", closeSidebar);
+	if(closeBtn){
+		closeBtn.addEventListener("click", closeSidebar);
+	}
+
+	/* Close sidebar when clicking any link */
+	sidebarLinks.forEach(link => {
+		link.addEventListener("click", () => {
+			closeSidebar();
+		});
 	});
 
 	function closeSidebar(){
-	sidebar.classList.remove("active");
-	overlay.classList.remove("active");
-	menuBtn.classList.remove("active");
+		sidebar.classList.remove("active");
+		overlay.classList.remove("active");
+		menuBtn.classList.remove("active");
 	}
 
-
-
-
-
 });
-
-

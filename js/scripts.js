2d81375b789c818733ff98f207420e715ef71c52
shohelAhmed <shohailislamshohel@gmail.com>
2026-03-09 13:46:53 +0600

    Last version of website

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-
+	
 const menuBtn = document.getElementById("menuBtn");
 const sidebar = document.getElementById("sidebar");
 const overlay = document.getElementById("overlay");
@@ -7,31 +7,29 @@ const closeBtn = document.getElementById("closeBtn");
 const sidebarLinks = document.querySelectorAll("#sidebar a");
 
 menuBtn.addEventListener("click", () => {
-
     sidebar.classList.toggle("active");
     overlay.classList.toggle("active");
     menuBtn.classList.toggle("active");
-
     document.body.classList.toggle("no-scroll");
-
 });
 
 overlay.addEventListener("click", closeSidebar);
 
-if(closeBtn){
+if (closeBtn) {
     closeBtn.addEventListener("click", closeSidebar);
 }
 
-sidebarLinks.forEach(link=>{
+sidebarLinks.forEach(link => {
     link.addEventListener("click", closeSidebar);
 });
 
-function closeSidebar(){
+function closeSidebar() {
     sidebar.classList.remove("active");
     overlay.classList.remove("active");
     menuBtn.classList.remove("active");
-
     document.body.classList.remove("no-scroll");
 }
+
+
 
 });
